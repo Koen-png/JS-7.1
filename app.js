c34504a8dcc52fc37dbe30a5d282734ba95ab7f4
Koen-png <dekoenduin@gmail.com>
2023-10-24 13:07:22 +0200

    that joinky sploinky

---
+++ app.js
@@ -0,0 +1,10 @@
+let a = undefined;
+let b = undefined;
+function calculate(a, b){
+    if (a < b){
+        alert ("Helaas, te weinig geldt (<- misspelling on the site)")
+    } else {
+        alert("U heeft genoeg geld!")
+    }
+
+}
