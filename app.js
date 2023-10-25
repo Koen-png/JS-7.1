494faa8d78a398626348e71da604109ef23e5b03
Koen-png <dekoenduin@gmail.com>
2023-10-25 11:15:02 +0200

    kapow

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-let a = undefined;
-let b = undefined;
-function calculate(a, b){
+function calculate(){
+    let a = parseFloat(prompt("vul uw budget in"));
+    let b = parseFloat(prompt("vul de kosten in"));
     if (a < b){
         alert ("Helaas, te weinig geldt (<- misspelling on the site)")
     } else {
         alert("U heeft genoeg geld!")
-    }
-
+    };
 }
