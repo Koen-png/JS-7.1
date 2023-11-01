08e41f9598900ce6b1c659e1d1b41ebbe7f635ea
Koen-png <dekoenduin@gmail.com>
2023-11-01 14:36:14 +0100

    8.1

--- app.js
+++ app.js
@@ -8,12 +8,47 @@ function calculate(){
     };
 }
 
+//this is just nicer than making a lot of files
+
 let clickercount = 0;
 function clicker(){
     clickercount ++;
     let thingamajig = document.getElementById("clicker");
-    thingamajig.textContent = "clicker count: " + clickercount
+    thingamajig.textContent = "clicker count: " + clickercount;
     if (clickercount > 10){
         clickercount = 0;
     }
+}
+
+let answer;
+function AddNumber(){
+    let a = parseFloat(document.getElementById("number1").value);
+    let b = parseFloat(document.getElementById("number2").value);
+    answer = document.getElementById('answerCalc');
+    answer.textContent = a + b;
+    return false;
+}
+
+function SubNumber(){
+    let a = parseFloat(document.getElementById("number1").value);
+    let b = parseFloat(document.getElementById("number2").value);
+    answer = document.getElementById('answerCalc');
+    answer.textContent = a - b;
+    return false;
+}
+
+function MultNumber(){
+    let a = parseFloat(document.getElementById("number1").value);
+    let b = parseFloat(document.getElementById("number2").value);
+    answer = document.getElementById('answerCalc');
+    answer.textContent = a * b;
+    return false;
+}
+
+function DivNumber(){
+    let a = parseFloat(document.getElementById("number1").value);
+    let b = parseFloat(document.getElementById("number2").value);
+    answer = document.getElementById('answerCalc');
+    answer.textContent = a / b;
+    return false;
 }
